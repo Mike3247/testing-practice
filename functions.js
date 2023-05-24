efdaaa5e180eea1be34eeb075f4ba65da2261eac
Mike Selmani <mike_selmani@hotmail.com>
2023-05-24 13:18:54 -0400

    reverseString method test and writing and passing thereof

---
+++ functions.js
@@ -0,0 +1,18 @@
+function sum(a, b) {
+    return a + b;
+  }
+
+function substract(a, b) {
+    return a - b;
+}
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() +
+    string.slice(1);
+}
+
+function reverseString(string) {
+    return string.split("").reverse().join("");
+}
+
+module.exports = {sum, substract, capitalize, reverseString};
---
+++ functions.test.js
@@ -0,0 +1,19 @@
+const { sum, substract, capitalize, reverseString } = require("./functions");
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+// const { substract } = require("./sum");
+
+test("substracts 5 - 3 to equal 2", () => {
+    expect(substract(5, 3)).toBe(2);
+});
+
+test("change first character of 'abc' to equal 'A'", () => {
+    expect(capitalize("abc")).toBe("Abc");
+});
+
+test("reverse 'abc' to equal 'cba'", () => {
+  expect(reverseString("abc")).toBe("cba");
+});
