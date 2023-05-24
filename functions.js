0de21bb9dbb6ebc3c78569f3a34ea0a2621640f7
Mike Selmani <mike_selmani@hotmail.com>
2023-05-24 13:54:38 -0400

    write functions and passing tests for reverseString and calculator

--- functions.js
+++ functions.js
@@ -1,10 +1,10 @@
-function sum(a, b) {
-    return a + b;
-  }
+// function sum(a, b) {
+//     return a + b;
+//   }
 
-function substract(a, b) {
-    return a - b;
-}
+// function subtract(a, b) {
+//     return a - b;
+// }
 
 function capitalize(string) {
     return string.charAt(0).toUpperCase() +
@@ -15,4 +15,23 @@ function reverseString(string) {
     return string.split("").reverse().join("");
 }
 
-module.exports = {sum, substract, capitalize, reverseString};
+function calculator(a, operation, b) { 
+    if (operation === "+") {
+        let add = a + b;
+        return add;
+    }
+    if (operation === "-") {
+        let subtract = a - b;
+        return subtract;
+    }
+    if (operation === "*") {
+        let multiply = a * b;
+        return multiply;
+    }
+    if (operation === "/") {
+        let divide = a / b;
+        return divide;
+    }
+}
+
+module.exports = { capitalize, reverseString, calculator};
--- functions.test.js
+++ functions.test.js
@@ -1,14 +1,12 @@
-const { sum, substract, capitalize, reverseString } = require("./functions");
+const { capitalize, reverseString, calculator } = require("./functions");
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
-});
+// test("adds 1 + 2 to equal 3", () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+// test("subtracts 5 - 3 to equal 2", () => {
+//     expect(subtract(5, 3)).toBe(2);
+// });
 
-// const { substract } = require("./sum");
-
-test("substracts 5 - 3 to equal 2", () => {
-    expect(substract(5, 3)).toBe(2);
-});
 
 test("change first character of 'abc' to equal 'A'", () => {
     expect(capitalize("abc")).toBe("Abc");
@@ -16,4 +14,17 @@ test("change first character of 'abc' to equal 'A'", () => {
 
 test("reverse 'abc' to equal 'cba'", () => {
   expect(reverseString("abc")).toBe("cba");
+});
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(calculator(1, "+", 2)).toBe(3);
+});
+test("subtracts 5 - 3 to equal 2", () => {
+  expect(calculator(5, "-", 3)).toBe(2);
+});
+test("multiplies 2 * 5 to equal 10", () => {
+  expect(calculator(2, "*", 5)).toBe(10);
+});
+test("divides 10 - 2 to equal 5", () => {
+  expect(calculator(10, "/", 2)).toBe(5);
 });
